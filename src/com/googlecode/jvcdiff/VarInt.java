649d646a96b07a217d11f610fb9a453cd34f8e55
David Ehrmann <ehrmann@gmail.com>
2011-07-19 06:43:13 +0000

    Initial commit

---
+++ src/com/googlecode/jvcdiff/VarInt.java
@@ -0,0 +1,14 @@
+package com.googlecode.jvcdiff;
+
+import java.nio.ByteBuffer;
+
+public class VarInt {
+
+	public static int getInt(ByteBuffer buffer) {
+		return 0;
+	}
+	
+	public static long getLong(ByteBuffer buffer) {
+		return 0;
+	}
+}

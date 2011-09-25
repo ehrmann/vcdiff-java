7e904704c31d8b1dab40354d241e3d351b5fe17b
ehrmann@gmail.com <ehrmann@gmail.com@463f0376-bb7f-8a62-fc81-d743ce61e1bf>
2011-09-25 21:43:10 +0000

    * Finish support for VarLong * Add CalculateLength methods * Expand test cases

--- trunk/src/com/googlecode/jvcdiff/VarIntTest.java
+++ trunk/src/com/googlecode/jvcdiff/VarIntTest.java
@@ -45,10 +45,13 @@ public class VarIntTest {
 	public void randomIntTest() throws VarIntParseException, VarIntEndOfBufferException {
 		Random random = new Random(0x42);
 		
-		ByteBuffer buffer = ByteBuffer.allocate(8192);
+		ByteBuffer buffer = ByteBuffer.allocate(16384);
 		
 		while (buffer.remaining() >= 5) {
-			VarInt.putInt(buffer, random.nextInt());
+			int startPos = buffer.position();
+			int testVal = random.nextInt();
+			VarInt.putInt(buffer, testVal);
+			Assert.assertEquals(buffer.position() - startPos, VarInt.calculateIntLength(testVal));
 		}
 		
 		buffer.flip();
@@ -56,6 +59,57 @@ public class VarIntTest {
 		random = new Random(0x42);
 		while (buffer.hasRemaining()) {
 			Assert.assertEquals(random.nextInt(), VarInt.getInt(buffer));
+		}
+	}
+	
+	@Test
+	public void basicLongTest() throws VarIntParseException, VarIntEndOfBufferException {
+		ByteBuffer buffer = ByteBuffer.allocate(64);
+		
+		buffer.clear();
+		VarInt.putLong(buffer, 0);
+		buffer.flip();
+		Assert.assertEquals(0, VarInt.getLong(buffer));
+		
+		buffer.clear();
+		VarInt.putLong(buffer, Long.MAX_VALUE);
+		buffer.flip();
+		Assert.assertEquals(Long.MAX_VALUE, VarInt.getLong(buffer));
+		
+		buffer.clear();
+		VarInt.putLong(buffer, Long.MIN_VALUE);
+		buffer.flip();
+		Assert.assertEquals(Long.MIN_VALUE, VarInt.getLong(buffer));
+		
+		buffer.clear();
+		VarInt.putLong(buffer, 1);
+		buffer.flip();
+		Assert.assertEquals(1, VarInt.getLong(buffer));
+		
+		buffer.clear();
+		VarInt.putLong(buffer, -1);
+		buffer.flip();
+		Assert.assertEquals(-1, VarInt.getLong(buffer));
+	}
+	
+	@Test
+	public void randomLongTest() throws VarIntParseException, VarIntEndOfBufferException {
+		Random random = new Random(0x42);
+		
+		ByteBuffer buffer = ByteBuffer.allocate(16384);
+		
+		while (buffer.remaining() >= 10) {
+			int startPos = buffer.position();
+			long testVal = random.nextLong();
+			VarInt.putLong(buffer, testVal);
+			Assert.assertEquals(buffer.position() - startPos, VarInt.calculateLongLength(testVal));
+		}
+		
+		buffer.flip();
+		
+		random = new Random(0x42);
+		while (buffer.hasRemaining()) {
+			Assert.assertEquals(random.nextLong(), VarInt.getLong(buffer));
 		}
 	}
 }

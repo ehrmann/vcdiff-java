11d0c28c9bc737356f2176b85005437cb79288c1
ehrmann@gmail.com <ehrmann@gmail.com@463f0376-bb7f-8a62-fc81-d743ce61e1bf>
2011-09-27 16:43:53 +0000

    Test against a known encoded form

--- trunk/src/com/googlecode/jvcdiff/VarIntTest.java
+++ trunk/src/com/googlecode/jvcdiff/VarIntTest.java
@@ -1,5 +1,7 @@
 package com.googlecode.jvcdiff;
 
+import static org.junit.Assert.assertArrayEquals;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -16,40 +18,40 @@ public class VarIntTest {
 	@Test
 	public void basicIntTest() throws VarIntParseException, VarIntEndOfBufferException {
 		ByteBuffer buffer = ByteBuffer.allocate(64);
-		
+
 		buffer.clear();
 		VarInt.putInt(buffer, 0);
 		buffer.flip();
 		Assert.assertEquals(0, VarInt.getInt(buffer));
-		
+
 		buffer.clear();
 		VarInt.putInt(buffer, Integer.MAX_VALUE);
 		buffer.flip();
 		Assert.assertEquals(Integer.MAX_VALUE, VarInt.getInt(buffer));
-		
+
 		buffer.clear();
 		VarInt.putInt(buffer, Integer.MIN_VALUE);
 		buffer.flip();
 		Assert.assertEquals(Integer.MIN_VALUE, VarInt.getInt(buffer));
-		
+
 		buffer.clear();
 		VarInt.putInt(buffer, 1);
 		buffer.flip();
 		Assert.assertEquals(1, VarInt.getInt(buffer));
-		
+
 		buffer.clear();
 		VarInt.putInt(buffer, -1);
 		buffer.flip();
 		Assert.assertEquals(-1, VarInt.getInt(buffer));
 	}
-	
+
 	@Test
 	public void randomIntTest() throws VarIntParseException, VarIntEndOfBufferException, IOException {
 		Random random = new Random(0x42);
-		
+
 		ByteBuffer buffer = ByteBuffer.allocate(16384);
 		ByteArrayOutputStream out = new ByteArrayOutputStream(buffer.remaining());
-		
+
 		while (buffer.remaining() >= 5) {
 			int startPos = buffer.position();
 			int testVal = random.nextInt();
@@ -57,54 +59,54 @@ public class VarIntTest {
 			VarInt.writeInt(out, testVal);
 			Assert.assertEquals(buffer.position() - startPos, VarInt.calculateIntLength(testVal));
 		}
-		
+
 		buffer.flip();
-		
+
 		Assert.assertEquals(0, buffer.compareTo(ByteBuffer.wrap(out.toByteArray())));
-		
+
 		random = new Random(0x42);
 		while (buffer.hasRemaining()) {
 			Assert.assertEquals(random.nextInt(), VarInt.getInt(buffer));
 		}
 	}
-	
+
 	@Test
 	public void basicLongTest() throws VarIntParseException, VarIntEndOfBufferException {
 		ByteBuffer buffer = ByteBuffer.allocate(64);
-		
+
 		buffer.clear();
 		VarInt.putLong(buffer, 0);
 		buffer.flip();
 		Assert.assertEquals(0, VarInt.getLong(buffer));
-		
+
 		buffer.clear();
 		VarInt.putLong(buffer, Long.MAX_VALUE);
 		buffer.flip();
 		Assert.assertEquals(Long.MAX_VALUE, VarInt.getLong(buffer));
-		
+
 		buffer.clear();
 		VarInt.putLong(buffer, Long.MIN_VALUE);
 		buffer.flip();
 		Assert.assertEquals(Long.MIN_VALUE, VarInt.getLong(buffer));
-		
+
 		buffer.clear();
 		VarInt.putLong(buffer, 1);
 		buffer.flip();
 		Assert.assertEquals(1, VarInt.getLong(buffer));
-		
+
 		buffer.clear();
 		VarInt.putLong(buffer, -1);
 		buffer.flip();
 		Assert.assertEquals(-1, VarInt.getLong(buffer));
 	}
-	
+
 	@Test
 	public void randomLongTest() throws VarIntParseException, VarIntEndOfBufferException, IOException {
 		Random random = new Random(0x42);
-		
+
 		ByteBuffer buffer = ByteBuffer.allocate(16384);
 		ByteArrayOutputStream out = new ByteArrayOutputStream(buffer.remaining());
-		
+
 		while (buffer.remaining() >= 10) {
 			int startPos = buffer.position();
 			long testVal = random.nextLong();
@@ -112,14 +114,33 @@ public class VarIntTest {
 			VarInt.writeLong(out, testVal);
 			Assert.assertEquals(buffer.position() - startPos, VarInt.calculateLongLength(testVal));
 		}
-		
+
 		buffer.flip();
-		
+
 		Assert.assertEquals(0, buffer.compareTo(ByteBuffer.wrap(out.toByteArray())));
-		
+
 		random = new Random(0x42);
 		while (buffer.hasRemaining()) {
 			Assert.assertEquals(random.nextLong(), VarInt.getLong(buffer));
 		}
+	}
+
+	@Test
+	public void longMatchesSpecTest() {
+		ByteBuffer buffer = ByteBuffer.allocate(16);
+		VarInt.putLong(buffer, 0xffffffffL);
+
+		int size = buffer.position();
+		buffer.flip();
+		byte[] data = new byte[size];
+		buffer.get(data);
+
+		assertArrayEquals(new byte[] {
+				(byte)0x8F,
+				(byte)0xFF,
+				(byte)0xFF,
+				(byte)0xFF,
+				0x7F,
+		}, data);
 	}
 }
